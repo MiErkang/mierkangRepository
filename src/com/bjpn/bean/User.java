daaaa6f93b290a4f4a687fe258826108f250bd8d
大郎 <dalang@123.com>
2023-06-18 17:08:21 +0800

    User类添加getter/setter方法提交

--- src/com/bjpn/bean/User.java
+++ src/com/bjpn/bean/User.java
@@ -13,4 +13,28 @@ public class User {
 
     public User() {
     }
+
+    public int getuId() {
+        return uId;
+    }
+
+    public void setuId(int uId) {
+        this.uId = uId;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+
+    public int getuAge() {
+        return uAge;
+    }
+
+    public void setuAge(int uAge) {
+        this.uAge = uAge;
+    }
 }
