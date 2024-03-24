f3d16b850d5faf51fe2b1b8234a5f860864443ba
mierkang <969575745@qq.com>
2024-03-24 22:10:05 +0800

    0324-03| 新增一个Teacher类

---
+++ src/com/bjpn/bean/Teacher.java
@@ -0,0 +1,31 @@
+package com.bjpn.bean;
+
+public class Teacher {
+    private Integer sAge;
+    private Integer sId;
+    private String sName;
+
+    public Integer getsAge() {
+        return sAge;
+    }
+
+    public void setsAge(Integer sAge) {
+        this.sAge = sAge;
+    }
+
+    public Integer getsId() {
+        return sId;
+    }
+
+    public void setsId(Integer sId) {
+        this.sId = sId;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+}
