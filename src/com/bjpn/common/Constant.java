4aabc7a1c95d11373be4c5c43c4ddb39093d16f4
mierkang <969575745@qq.com>
2024-04-06 17:22:25 +0800

    0406-02| 开发：新增常量类

---
+++ src/com/bjpn/common/Constant.java
@@ -0,0 +1,17 @@
+package com.bjpn.common;
+
+public class Constant {
+    public static final String LOAN_HISTORY_RATE = "loanHistoryRate";
+    public static final String USER_COUNT = "userCount";
+    public static final String BID_MONEY_SUM = "bidMoneySum";
+
+    public static final String LOGIN_USER = "loginUser";
+
+    //投资相关状态码
+    public static final int BID_LOAN_MONEY_REDUCE_FALL = 50003;
+    public static final int BID_LOAN_STATUS_UPDATE_FALL = 50002;
+    public static final int BID_FINANCE_MONEY_REDUCE_FALL = 40002;
+    public static final int BID_MONEY_INPUT_FALL = 40003;
+    public static final int BID_INFO_FALL = 50000;
+    public static final int BID_INFO_SUCCESS = 20000;
+}
