09b4b167dfb1f58465f2d4776fd5b650eddaff7f
mierkang <969575745@qq.com>
2024-04-06 17:49:50 +0800

    0406-04| 修复：增加数组转字符串方法

--- src/com/bjpn/strDeal/StrDemo.java
+++ src/com/bjpn/strDeal/StrDemo.java
@@ -5,6 +5,8 @@ public class StrDemo {
         String a = "a&s&d&123";
         String b = "qwe456";
         strTest(a);
+        String[] str1 = {"a", "b", "c", "1", "2", "3"};
+        strTest01(str1);
     }
 
     //字符串转数组
@@ -13,5 +15,15 @@ public class StrDemo {
         for (int i = 0; i < strings.length; i++) {
             System.out.println("strings = " + strings[i]);
         }
+    }
+
+    //数组转字符串
+    public static void strTest01(String[] strings) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (String s :
+                strings) {
+            stringBuffer.append(s);
+        }
+        System.out.println("stringBuffer = " + stringBuffer.toString());
     }
 }
